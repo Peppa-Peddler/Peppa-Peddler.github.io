3f050c10eda020cc2f04bd0eb6c240cad6bdba93
peppa-peddler <peppasama@gmail.com>
2016-12-14 12:08:35 -0500

    fixing

---
+++ js/form.js
@@ -0,0 +1,15 @@
+$("#form0").show();
+$("#form1").hide();
+$("#form2").hide();
+$("#form3").hide();
+
+$(".btn-next").click(function(){
+	var number = $(this).attr("id").slice(-1);
+	number = +number + 1;
+	$("#form" + (number-1)).fadeOut(1000,function(){
+		$("#form" + number).fadeIn(800);
+	});
+	$('html, body').stop().animate({
+          'scrollTop': 0
+    }, 1000, 'swing');
+});
